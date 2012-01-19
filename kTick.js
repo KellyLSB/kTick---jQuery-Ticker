c829966ffbb88755cac26c834b052857db47977e
Kelly Becker <kellylsbkr@gmail.com>
2012-01-18 17:46:53 -0700

    Initial Commit

---
+++ kTick.js
@@ -0,0 +1,80 @@
+(function( $ ) {
+
+	// Prepare a list of running tickers
+	var running = new Object();
+			
+	$.fn.kTick = function( options ) {
+
+		if(options == 'stop') {
+			clearInterval(running[this]);
+			return;
+		}
+
+		// Template Renderer
+		var render = function(p, obj) {
+			var pattern = p;
+
+			for(var x in obj) {
+				pattern = pattern.replace('%'+x+'%', obj[x]);
+			}
+
+			for(var x in obj) {
+				pattern = pattern.replace('%'+x+'%', obj[x]);
+			}
+
+			return pattern;
+		};
+
+		// Check If Is String
+		var isString = function(input){
+			return typeof(input) == 'string';
+		}
+
+		// Set the object to a local variable
+		var obj = this;
+		
+		// Settings Extensions
+		var settings = $.extend( {
+			'timeout': 1000,
+			'animation': 'fade', //  Fade or Slide
+			'animation_time': 1500,
+			'template': '',
+			'json': '',
+			'jsonArgs': {},
+			'loop': true,
+			'truncate': true
+		} , options);
+
+		// If is a string then assume url and load the json
+		if(isString(settings.json)) {
+			$.get(settings.json, settings.jsonArgs, function(data) {
+				settings.json = data;
+			});
+		}
+
+		var x = 0;
+		var loop = false;
+		running[obj] = setInterval(function() {
+			// If this is a loop and we are truncating remove the last element
+			if(settings.truncate == true && loop == true) {
+				var tmp = obj.children().last();
+				if(settings.animation == 'fade') tmp.fadeOut(settings.animation_time);
+				if(settings.animation == 'slide') tmp.slideUp(settings.animation_time);
+				tmp.remove();
+			}
+
+			// Create First Line In Ticker
+			var tmp = $(render(settings.template, settings.json[x])).css('display', 'none');
+
+			// Prepend and Animate
+			obj.prepend(tmp); x++;
+			if(settings.animation == 'fade') tmp.fadeIn(settings.animation_time);
+			if(settings.animation == 'slide') tmp.slideDown(settings.animation_time);
+			if(x == settings.json.length) {
+				if(settings.loop !== true) clearInterval(running[obj]);
+				loop = true;
+				x = 0;
+			}
+		}, settings.timeout);
+	};
+})( jQuery );
---
+++ readme.md
@@ -0,0 +1,24 @@
+kTick - jQuery Ticker (https://github.com/KellyLSB/Confirmation-Code-Generator)
+===============================================================================
+
+kTick is a simple jQuery ticker that parses and spits out a JSON array into whatever html you see fit.
+
+### The available paramaters are.
+
+	{
+		'timeout': 1000,			// The amount of time between new ticks
+		'animation': 'fade',		// Fade or Slide the new tick in
+		'animation_time': 1500,		// Time it takes to Fade or Slide
+		'template': '',				// The HTML Template with variables surounded by "%"
+		'json': '',					// URL to a JSON feed or a JSON object to loop through
+		'jsonArgs': {}				// Arguments to add to the JSON Get call (URL FEED ONLY)
+		'loop': true,				// To loop through the JSON object continuously
+		'truncate': true,			// If looping then remove the last item when adding it to the top
+	}
+
+License
+=======
+Confirmation Code Generator by Kelly Lauren Summer Becker is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
+Permissions beyond the scope of this license may be available at http://kellybecker.me.
+
+[<img src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" alt="BY-SA" title="BY-SA">](http://creativecommons.org/licenses/by-sa/3.0/)
